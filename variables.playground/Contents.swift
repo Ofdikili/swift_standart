20518e77e404223639977c7233fc687c27869d7f
omer faruk <odikili@stilus.com.tr>
2022-04-22 22:18:25 +0300

    değişkenler eklendi

---
+++ variables.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var name : String = "Omer";
+var age  : Int = 26;
+var height : Double = 1.78 ;
+var weight : Double = 80 ;
+var isFemale : Bool = false ;
+var speacialLetter : Character = "s";
+var job : String? ;
+
+print(name , age , height , weight ,  isFemale,  speacialLetter , separator: "-")
+job = "Computer Engineer"
+print("Kullanıcı Adı : \(name) , Yaşı : \(age)")
+
+print(job!);
+var numberOne = 10 ;
+var numberTwo =  20 ;
+var totalNumber = numberOne + numberTwo ;
+print(totalNumber);
