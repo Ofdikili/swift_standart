46c5dfc01c865a9fa9bf4844118261f7341a1f09
omer faruk <odikili@stilus.com.tr>
2022-04-23 12:04:22 +0300

    değişkenler bölümüne tuples eklendi

--- variables.playground/Contents.swift
+++ variables.playground/Contents.swift
@@ -17,3 +17,28 @@ var numberOne = 10 ;
 var numberTwo =  20 ;
 var totalNumber = numberOne + numberTwo ;
 print(totalNumber);
+
+//tuples
+
+var student = ("ÖMER Faruk Dikili" , 25 , true , 10.8)
+var studentName : String = student.0;
+var studentAge :Int = student.1;
+var studentIsGraduate : Bool = student.2
+
+print("Öğrenci Adı : \(studentName) , Öğrenci Yaşı : \(studentAge) , Öğrenci Mezun mu : \(studentIsGraduate)")
+
+var (newStudentName , newStudentAge , newStudentIsGraduate , newStudentAvarage)  = student
+print("Yeni Öğrenci Adı : \(newStudentName) , Yeni Öğrenci Yaşı : \(newStudentAge) , Yeni Öğrenci Mezun mu : \(newStudentIsGraduate)")
+
+var shapeAreas = (circle : 30 , square : 40 , pentagon : 60 , hexagon : 80)
+shapeAreas.square = 300
+print("Circle of Area : \(shapeAreas.circle)")
+print("Square of Area : \(shapeAreas.square)")
+
+//nested tuples
+
+var manager = ("ÖFD" , ("NEW" , 10))
+
+print(manager.0)
+print(manager.1.0)
+print(manager.1.1)
